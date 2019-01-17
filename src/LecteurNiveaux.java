6a60cd52f82bcc54b75c44a0bca48086257b5937
ALLian96 <dntt66@hotmail.com>
2019-01-18 04:46:17 +0800

    Update LecteurNiveaux.java

--- src/LecteurNiveaux.java
+++ src/LecteurNiveaux.java
@@ -9,7 +9,7 @@ public class LecteurNiveaux {
 			FileReader fr = null;
 			BufferedReader br = null;
 			try {
-				fr = new FileReader("H:\\java\\Sokoban-java\\src\\Original.txt");
+				fr = new FileReader("bin/Original.txt");
 				br  = new BufferedReader(fr);
 				String s  = "";
 				StringBuffer str = new StringBuffer();
