001e589a36da6d9e90086dea89239fb9c07cfef5
ALLian96 <dntt66@hotmail.com>
2019-01-17 21:02:55 +0100

    Update LecteurNiveaux.java

--- src/LecteurNiveaux.java
+++ src/LecteurNiveaux.java
@@ -15,7 +15,20 @@ public class LecteurNiveaux {
 				StringBuffer str = new StringBuffer();
 				boolean stop = false;
 				while((s=br.readLine())!=null && stop == false ) {
-					if(s.startsWith(";")) {
+					
+					if(s.charAt(0)==';') { 
+							
+						/*
+						 public String substring(int beginIndex, int endIndex)
+第一个int为开始的索引，对应String数字中的开始位置，
+第二个是截止的索引位置，对应String中的结束位置
+1、取得的字符串长度为：endIndex - beginIndex;
+2、从beginIndex开始取，到endIndex结束，从0开始数，其中不包括endIndex位置的字符
+如：
+"hamburger".substring(4, 8) returns "urge"
+ "smiles".substring(1, 5) returns "mile"
+取长度大于等于3的字符串a的后三个子字符串，只需a.subString(a.length()-3, a.length());
+						 */
 						stop = true;
 					}
 					System.out.println(s);
